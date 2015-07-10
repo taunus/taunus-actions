fb2bffbbf86d34ef69e3b833437e66a0d2b58c4e
JC Ivancevich <jc.ivancevich@gmail.com>
2015-07-10 13:45:52 -0300

    send route if defined + little refactor

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ var breadcrumb = require('breadcrumb');
 function configure (options) {
   var taunus = options.taunus;
 
-  taunus.on('render', function (container, viewModel) {
+  taunus.on('render', function (container, viewModel, route) {
     $('[data-taunus-action]', container).forEach(function (el) {
       var $el = $(el);
       var action = $el.attr('data-taunus-action');
-      var model = $el.attr('data-taunus-model');
+      var crumbs = $el.attr('data-taunus-model');
+
+      var model = crumbs ? breadcrumb(viewModel, crumbs) : viewModel;
       var controller = taunus.state.controllers[action];
 
       if (!controller) {
-        throw new Error('Taunus action not found');
+        global.DEBUG && global.DEBUG('[actions] Action not found', action, el); return;
       }
 
-      var componentModel = viewModel;
-      if (model) {
-        componentModel = breadcrumb(viewModel, model);
-      }
-
-      controller(componentModel, $el);
+      controller(model, $el, route || null);
     });
   });
 }
