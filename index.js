cdb9ed71d26ee941bc2494447a1e18b88c28132a
JC Ivancevich <jc.ivancevich@gmail.com>
2015-07-10 13:58:49 -0300

    little fixes

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ function configure (options) {
     $('[data-taunus-action]', container).forEach(function (el) {
       var $el = $(el);
       var action = $el.attr('data-taunus-action');
-      var crumbs = $el.attr('data-taunus-model');
-
-      var model = crumbs ? breadcrumb(viewModel, crumbs) : viewModel;
       var controller = taunus.state.controllers[action];
 
       if (!controller) {
         global.DEBUG && global.DEBUG('[actions] Action not found', action, el); return;
       }
 
-      controller(model, $el, route || null);
+      var crumbs = $el.attr('data-taunus-model');
+      var model = crumbs ? breadcrumb(viewModel, crumbs) : viewModel;
+
+      controller(model, el, route);
     });
   });
 }
